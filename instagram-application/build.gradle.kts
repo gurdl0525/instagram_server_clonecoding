a72af9ce0b37567518a5c74f47f4414b0bf2482d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-23 22:49:40 +0900

    📌 :: project settings

---
+++ instagram-application/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("plugin.allopen") version "1.6.21"
+}
+
+
+dependencies {
+    implementation(project(":instagram-domain"))
+    implementation("org.springframework:spring-tx:6.0.3")
+}
+
+allOpen {
+    annotation("com.example.global.annotation.UseCase")
+}
