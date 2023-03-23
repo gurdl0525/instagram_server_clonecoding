f3891ee621766587b7b1d14601f286903b7c8ec8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-23 22:49:07 +0900

    📝 :: project settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,53 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.5"
-    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        version = "1.6.21"
+    }
+    apply {
+        plugin("org.jetbrains.kotlin.kapt")
+        version = "1.7.10"
+    }
 
-repositories {
-    mavenCentral()
-}
+    repositories {
+        mavenCentral()
+    }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+allprojects {
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "17"
+            }
+        }
+
+        compileJava {
+            sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+        }
+    }
+
+    repositories {
+        mavenCentral()
+    }
 }
