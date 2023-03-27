55a769d24f40258bbbac286f568a0ee34dee7f40
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-27 18:53:19 +0900

    📌 :: UserRepository

---
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/domain/persistense/user/repository/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.example.instagram.domain.persistense.user.repository
+
+import com.example.instagram.domain.persistense.user.entity.UserJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface UserRepository: JpaRepository<UserJpaEntity, UUID> {
+
+    fun findByAccountId(accountId: String): UserJpaEntity?
+}
