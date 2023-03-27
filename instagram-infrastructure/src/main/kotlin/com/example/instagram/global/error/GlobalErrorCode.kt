2d0d430e09913ef4edfc0ec5d8a7336a07e53ae1
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-27 18:53:42 +0900

    📌 :: GlobalErrorCode

---
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/error/GlobalErrorCode.kt
@@ -0,0 +1,27 @@
+package com.example.instagram.global.error
+
+import org.springframework.http.HttpStatus
+
+enum class GlobalErrorCode(
+    private val status: HttpStatus,
+    private val code: String,
+    private val message: String
+): ErrorProperty {
+
+    //400
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "B01", "Bad Request"),
+
+    //401
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "U01", "Invalid Token"),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "U02", "Expired Token"),
+
+    //500
+    INTERNAL_SERVER_GLOBAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "I01", "Internal Server Error")
+    ;
+
+    override fun status() = status
+
+    override fun code() = code
+
+    override fun message() = message
+}
