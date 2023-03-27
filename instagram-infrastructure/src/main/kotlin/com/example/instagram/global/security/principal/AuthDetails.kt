be2d4d77fa6771f8bf8260301337110e9c682211
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-27 18:54:45 +0900

    📌 :: AuthDetails

---
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/security/principal/AuthDetails.kt
@@ -0,0 +1,24 @@
+package com.example.instagram.global.security.principal
+
+import com.example.instagram.domain.persistense.user.entity.UserJpaEntity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: UserJpaEntity
+): UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = ArrayList()
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.accountId
+
+    override fun isAccountNonExpired(): Boolean = !user.isDeleted
+
+    override fun isAccountNonLocked(): Boolean = !user.isDeleted
+
+    override fun isCredentialsNonExpired(): Boolean = false
+
+    override fun isEnabled(): Boolean = !user.isDeleted
+}
