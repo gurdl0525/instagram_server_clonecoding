51bea774cfa695c76906a9169b68bff7295d31f7
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-27 18:55:12 +0900

    📌 :: AuthDetailsService

---
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/security/principal/AuthDetailsService.kt
@@ -0,0 +1,18 @@
+package com.example.instagram.global.security.principal
+
+import com.example.instagram.domain.persistense.user.repository.UserRepository
+import com.example.instagram.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class AuthDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(accountId: String): UserDetails {
+        return AuthDetails(userRepository.findByAccountId(accountId)
+            ?: throw UserNotFoundException)
+    }
+}
