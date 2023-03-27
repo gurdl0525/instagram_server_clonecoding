f67fd661c023787ecfe79dc54c7b4739430c232c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-27 18:54:30 +0900

    📌 :: JwtProperty

---
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/jwt/property/JwtProperties.kt
@@ -0,0 +1,17 @@
+package com.example.instagram.global.jwt.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.util.Base64
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties(
+    secretKey: String,
+    accessExp: Int,
+    refreshExp: Int
+) {
+    val secretKey: String = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+    val accessExp: Int = accessExp * 1000
+    val refreshExp: Int = refreshExp * 1000
+}
