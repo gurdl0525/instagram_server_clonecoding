42106eb7883ea07e21e951f476629599a209e457
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-29 17:05:10 +0900

    📝 :: MutableList -> List

--- instagram-infrastructure/src/main/kotlin/com/example/instagram/domain/persistense/user/converter/StringListConverter.kt
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/domain/persistense/user/converter/StringListConverter.kt
@@ -1,23 +1,24 @@
 package com.example.instagram.domain.persistense.user.converter
 
-import java.util.Arrays
-import java.util.Arrays.asList
 import javax.persistence.AttributeConverter
 import javax.persistence.Converter
 
 @Converter
-class StringListConverter: AttributeConverter<MutableList<String>, String> {
+class StringListConverter: AttributeConverter<List<String>, String> {
 
     companion object{
         private const val SPLIT_CHAR: String = ";"
     }
 
-    override fun convertToDatabaseColumn(stringList: MutableList<String>): String {
-        return String.format(SPLIT_CHAR, stringList)
+    override fun convertToDatabaseColumn(stringList: List<String>?): String? {
+        return stringList?.let {
+            String.format(SPLIT_CHAR, stringList)
+        }?.toString()
     }
 
-    override fun convertToEntityAttribute(string: String): MutableList<String> {
-        return string.split(SPLIT_CHAR).toMutableList()
+    override fun convertToEntityAttribute(string: String?): List<String>? {
+        return string?.split(SPLIT_CHAR)?.toList()
+
     }
 
 
