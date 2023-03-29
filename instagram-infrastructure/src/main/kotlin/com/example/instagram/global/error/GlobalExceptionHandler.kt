592e6b6233869ee61545a0d7569f82c2ec7b903a
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-29 17:03:45 +0900

    🧪 :: handleHttpMessageNotReadable

--- instagram-infrastructure/src/main/kotlin/com/example/instagram/global/error/GlobalExceptionHandler.kt
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/error/GlobalExceptionHandler.kt
@@ -1,6 +1,8 @@
 package com.example.instagram.global.error
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.BindException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -11,13 +13,22 @@ class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException::class)
-    protected fun handleBindException(e: BindException): BindErrorResponse? = ErrorResponse.of(e)
+    protected fun handleBindException(e: BindException): BindErrorResponse = ErrorResponse.of(e)
+
 
     @ExceptionHandler(BusinessException::class)
-    protected fun customExceptionHandle(e: BusinessException) = ErrorResponse(
-        e.errorProperty.status(),
-        e.errorProperty.code(),
-        e.errorProperty.message()
+    protected fun customExceptionHandle(e: BusinessException) = handleException(e)
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    protected fun handleHttpMessageNotReadable(e: HttpMessageNotReadableException) = ErrorResponse(
+        HttpStatus.BAD_REQUEST,
+        e.httpInputMessage.toString(),
+        e.message ?: e.localizedMessage
     )
 
+    private fun handleException(e: BusinessException): ResponseEntity<ErrorResponse> {
+        val status = e.errorProperty.status()
+        val body = ErrorResponse.of(e.errorProperty)
+        return ResponseEntity(body, status)
+    }
 }
