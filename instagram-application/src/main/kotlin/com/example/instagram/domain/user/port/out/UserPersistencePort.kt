9cfc7211bf34fb1aa7666b12bf9a89daa81d27ed
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-29 17:01:05 +0900

    📌 :: UserPersistencePort

---
+++ instagram-application/src/main/kotlin/com/example/instagram/domain/user/port/out/UserPersistencePort.kt
@@ -0,0 +1,14 @@
+package com.example.instagram.domain.user.port.out
+
+import com.example.instagram.domain.auth.port.`in`.SignUpPort
+import com.example.instagram.domain.user.model.User
+import com.example.instagram.domain.user.port.`in`.UpdateProfilePort
+
+interface UserPersistencePort: SignUpPort, FindUserPort, UpdateProfilePort {
+
+    override fun saveUser(user: User)
+
+    override fun findByAccountId(accountId: String): User?
+
+    override fun updateProfile(user: User)
+}
