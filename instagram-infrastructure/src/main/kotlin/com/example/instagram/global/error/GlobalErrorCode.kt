e13786ee6b19392ffe8af61309f47f610d7faee8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-04-03 12:50:30 +0900

    📝 :: HttpStatus -> Int

--- instagram-infrastructure/src/main/kotlin/com/example/instagram/global/error/GlobalErrorCode.kt
+++ instagram-infrastructure/src/main/kotlin/com/example/instagram/global/error/GlobalErrorCode.kt
@@ -3,21 +3,21 @@ package com.example.instagram.global.error
 import org.springframework.http.HttpStatus
 
 enum class GlobalErrorCode(
-    private val status: HttpStatus,
+    private val status: Int,
     private val code: String,
     private val message: String
 ): ErrorProperty {
 
     //400
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "B01", "Bad Request"),
+    BAD_REQUEST(400, "B01", "Bad Request"),
 
     //401
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "U01", "Invalid Token"),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "U02", "Expired Token"),
-    UN_AUTHORIZED(HttpStatus.UNAUTHORIZED, "U03", "Un Authenticated"),
+    INVALID_TOKEN(401, "U01", "Invalid Token"),
+    EXPIRED_TOKEN(401, "U02", "Expired Token"),
+    UN_AUTHORIZED(401, "U03", "Un Authenticated"),
 
     //500
-    INTERNAL_SERVER_GLOBAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "I01", "Internal Server Error")
+    INTERNAL_SERVER_GLOBAL_ERROR(500, "I01", "Internal Server Error")
     ;
 
     override fun status() = status
