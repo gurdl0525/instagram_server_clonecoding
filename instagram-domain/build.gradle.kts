dce488a1344a33cc8539383c45b4da1c5e146bcf
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-04-03 12:49:32 +0900

    🗒️ :: 불필요한 Dependency

--- instagram-domain/build.gradle.kts
+++ instagram-domain/build.gradle.kts
@@ -1,13 +1,2 @@
-plugins{
-    id("org.springframework.boot") version "2.7.7"
-    id("io.spring.dependency-management") version "1.1.0"
-}
-
 dependencies {
-
-    implementation("org.springframework.boot:spring-boot-starter-web")
-}
-
-tasks.getByName<Jar>("bootJar") {
-    enabled = false
 }
